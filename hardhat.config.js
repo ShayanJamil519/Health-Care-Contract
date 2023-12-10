72a6c41b05f4f376b6c9a26100f52d0c33ec041d
Abdullah Aziz <abdullahazees4@gmail.com>
2023-12-10 18:55:03 +0500

    code added

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.METAMASK_ACCOUNT_PRIVATE_KEY;
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+// const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY;
 module.exports = {
   solidity: {
     compilers: [
@@ -17,6 +21,12 @@ module.exports = {
     localganache: {
       url: process.env.PROVIDER_URL,
       accounts: [process.env.PRIVATE_KEY],
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
     },
   },
 };
